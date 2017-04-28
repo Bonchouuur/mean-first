a099b76eac6cdbf2d6e513563234226d66110254
BONCHOUUUUR <n.chauris@gmail.com>
2017-04-28 10:29:17 +0200

    Replacing env by sample

---
+++ config/env/development.sample.js
@@ -0,0 +1,9 @@
+module.exports = {
+    sessionSecret: 'XXXXXXXXXXXX',
+    db: 'mongodb://localhost/BASE_NAME',
+    facebook: {
+        clientID: 'Application Id',
+        clientSecret: 'Application Secret',
+        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    }
+};
